a8a777ccb5e64d38a488ad306118c44fa32484c4
Alexander Bulbenkov <bualal2005@gmail.com>
2024-05-06 23:54:16 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,102 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+
+public class Main {
+    // поиск минимального числа в списке
+    public static int _min(int[] numbers) {
+        int min = numbers[0];
+        for (int number : numbers) {
+            if (number < min) {
+                min = number;
+            }
+        }
+        return min;
+    }
+
+    // поиск максимального числа в списке
+    public static int _max(int[] numbers) {
+        int max = numbers[0];
+        for (int number : numbers) {
+            if (number > max) {
+                max = number;
+            }
+        }
+        return max;
+    }
+
+    // сумма чисел из списка
+    public static int _sum(int[] numbers) {
+        int sum = 0;
+        for (int number : numbers) {
+            try {
+                sum = Math.addExact(sum, number);
+            } catch(ArithmeticException e) {
+                System.out.println("Very big nums, overflow " + e);
+                System.exit(0);
+            }
+        }
+        return sum;
+    }
+
+    // произведение чисел в списке
+    public static long _mult(int[] numbers) {
+        long product = 1;
+        for (int number : numbers) {
+            try {
+                product = Math.multiplyExact(product, number);
+            } catch(ArithmeticException e) {
+                System.out.println("Very big nums, overflow " + e);
+                System.exit(0);
+            }
+        }
+        return product;
+    }
+
+    // функция считывает числа из списка и выдает в формате массива
+    public static int[] readNumbers(Scanner scanner) {
+        String[] numberStrings = new String[0];
+        try {
+            numberStrings = scanner.nextLine().split(" ");
+        } catch (NoSuchElementException e) {
+            System.out.println("zero nums in file: " + e.getMessage());
+            System.exit(0);
+        }
+        int[] numbers = new int[numberStrings.length];
+        for (int i = 0; i < numberStrings.length; i++) {
+            try {
+                numbers[i] = Integer.parseInt(numberStrings[i]);
+            } catch (NumberFormatException e) {
+                System.out.println("invalid num format in file: " + e.getMessage());
+                System.exit(0);
+            }
+        }
+        return numbers;
+    }
+
+    public static void main(String[] args) {
+        String filename = "num.txt"; // имя файла
+        // Чтение чисел из файла
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(new File("src/" + filename));
+        } catch (FileNotFoundException e) {
+            System.out.println("file not found: " + e.getMessage());
+            System.exit(0);
+        }
+        int[] numbers = readNumbers(scanner);
+
+        try{
+            System.out.println("Минимальное: " + _min(numbers));
+            System.out.println("Максимальное: " + _max(numbers));
+            System.out.println("Сумма: " + _sum(numbers));
+            System.out.println("Произведение: " + _mult(numbers));
+        } catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("no elements in num list: " + e.getMessage());
+            System.exit(0);
+        }
+
+    }
+}
---
+++ src/MainTest.java
@@ -0,0 +1,67 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+    // Тесты одноименных функций
+    @Test
+    public void min() {
+        //создаем тестовые данные
+        int[] numbers = {1, 2, 3, 4, 1123902};
+
+        //запускаем тест, в случае если результат будет не равен 1, то
+        //тест будет провален, о результатах теста читаем в консоли
+        Assert.assertEquals(1, Main._min(numbers));
+    }
+
+    @Test
+    public void max() {
+        int[] numbers = {1, 2, 3, 4, 1123902};
+
+        Assert.assertEquals(1123902, Main._max(numbers));
+    }
+
+    @Test
+    public void sum() {
+        int[] numbers = {1, 2, 3, 4, 1123902};
+
+        Assert.assertEquals(1123912, Main._sum(numbers));
+    }
+
+    @Test
+    public void mul() {
+        int[] numbers = {1, 2, 3, 4, 5};
+
+        Assert.assertEquals(120, Main._mult(numbers));
+    }
+
+    // Тест на скорость
+    @Test
+    public void speed_test() {
+        int[] numbers = new int[100000000];
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = 1;
+        }
+        long start = System.currentTimeMillis();
+        Main._min(numbers);
+        Main._max(numbers);
+        Main._mult(numbers);
+        Main._sum(numbers);
+
+        long time = (System.currentTimeMillis() - start);
+        assertTrue(time <= 500);
+    }
+
+    // Тест на то, что если в массиве 0, то результат умножения 0
+    @Test
+    public void zero_mult() {
+        int[] numbers = {23432, -323423, 234234, 0};
+
+        assertEquals(0, Main._mult(numbers));
+    }
+}
